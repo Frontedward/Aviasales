64366d77ec675693a4b2023206a6250f6963009c
uglynoize <e.shabronsky@gmail.com>
2023-05-13 23:43:06 +0300

    debugging sc-module

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,7 +1,6 @@
 import 'styled-components';
 
 declare module 'styled-components' {
-  // eslint-disable-next-line
   export interface DefaultTheme {
     interactive: string;
     mainAccent: string;
